8b946f7d4c74d59a0a09aca8516ea17affbf6123
Rick Taylor <rick@tropicalstormsoftware.com>
2010-10-23 12:07:56 +0100

    Don't copy libtool files
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 echo Running autotools...
 aclocal -I m4 && \
 autoheader && \
-libtoolize --copy --force --no-warn && \
+libtoolize --force --no-warn && \
 automake --foreign --add-missing --copy && \
 autoconf
