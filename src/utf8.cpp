8fb5abc6b3e705b697e085b3042dab2199d58a2d
Rick Taylor <rick@tropicalstormsoftware.com>
2011-05-14 11:41:44 +0100

    New utf8 conversion with better support for overlong encoding
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- src/utf8.cpp
+++ src/utf8.cpp
@@ -178,91 +178,167 @@ size_t OOBase::to_native(char* sz, size_t len, const wchar_t* wsz, size_t wlen)
 
 #else
 
+#include <wchar.h>
+#include <stdlib.h>
+
 namespace
 {
-	static const int trailingBytesForUTF8[256] =
+	static const char utf8_data[256] =
 	{
-		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,   // 0x00 - 0x1F
-		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,   // 0x20 - 0x3F
-		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,   // 0x40 - 0x5F
-		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,   // 0x60 - 0x7F
-		9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9, 9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,   // 0x80 - 0x9F
-		9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9, 9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,   // 0xA0 - 0xBF
-		9,9,1,1,1,1,1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,   // 0xC0 - 0xDF
-		2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 3,3,3,3,3,11,11,11,4,4,4,4,5,5,6,7 // 0xE0 - 0xFF
+		// Key:
+		//  0 = Invalid first byte
+		//  1 = Single byte
+		//  2 = 2 byte sequence
+		//  3 = 3 byte sequence
+		//  4 = 4 byte sequence
+		
+		// -1 = Continuation byte
+		// -2 = Overlong 2 byte sequence
+		// -3 = 3 byte overlong check (0x80..0x9F) as next byte fail
+		// -4 = 3 byte reserved check (0xA0..0xBF) as next byte fail
+		// -5 = 4 byte overlong check (0x80..0x8F) as next byte fail
+		// -6 = 4 byte reserved check (0x90..0xBF) as next byte fail
+		
+		 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  // 0x00..0x0F
+		 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  // 0x10..0x1F
+		 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  // 0x20..0x2F
+		 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  // 0x30..0x3F
+		 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  // 0x40..0x4F
+		 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  // 0x50..0x5F
+		 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  // 0x60..0x6F
+		 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  // 0x70..0x7F
+		-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,  // 0x80..0x8F
+		-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,  // 0x90..0x9F
+		-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,  // 0xA0..0xAF
+		-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,  // 0xB0..0xBF
+		-2,-2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,  // 0xC0..0xCF
+		 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,  // 0xD0..0xDF
+		-3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,-4, 3, 3,  // 0xE0..0xEF
+		-5, 4, 4, 4,-6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0   // 0xF0..0xFF
 	};
+
+	static const unsigned int utf_subst_val = 0xFFFD;
 }
 
-size_t OOBase::measure_utf8(const char* sz, size_t len)
+size_t OOBase::measure_utf8(const char* start, size_t len)
 {
-	if (!sz)
+	if (!start)
 		return (len == size_t(-1) ? 1 : 0);
 
+	const char* sz = start;
 	size_t required_len = 0;
-	for (const char* p=sz; len == size_t(-1) ? *p!='\0' : size_t(p-sz)<len;)
+	unsigned char c;
+
+	do
 	{
-		unsigned int c;
-		unsigned char v = *p++;
-		int trailers = trailingBytesForUTF8[v];
-		switch (trailers)
+		// Default to substitution value
+		unsigned int wide_val = utf_subst_val;
+		int l = 0;
+
+		c = *sz++;
+		
+		switch (utf8_data[c])
 		{
-		case 0:
-			c = v;
-			break;
-
-		case 1:
-			c = v & 0x1f;
-			break;
-
-		case 2:
-			c = v & 0xf;
-			break;
-
-		case 3:
-			c = v & 0x7;
-			break;
-
-		default:
-			trailers &= 7;
-			c = L'\xFFFD';
+			case 1:
+				wide_val = c;
+				break;
+			
+			case 2: 
+				wide_val = (c & 0x1F);
+				l = 1;
+				break;
+			
+			case -2:
+				++sz;
+				break;
+				
+			case 3:
+				wide_val = (c & 0x0F);
+				l = 2;
+				break;
+			
+			case -3: 
+				if (*sz >= 0x80 && *sz <= 0x9F)
+					sz += 2;
+				else
+				{
+					wide_val = (c & 0x0F);
+					l = 2;
+				}
+				break;
+					
+			case -4:
+				if (*sz >= 0xA0 && *sz <= 0xBF)
+					sz += 2;
+				else
+				{
+					wide_val = (c & 0x0F);
+					l = 2;
+				}
+				break;
+				
+			case 4:
+				wide_val = (c & 0x07);
+				l = 3;
+				break;
+				
+			case -5:
+				if (*sz >= 0x80 && *sz <= 0x8F)
+					sz += 3;
+				else
+				{
+					wide_val = (c & 0x07);
+					l = 3;
+				}
+				break;
+			
+			case -6:
+				if (*sz >= 0x90 && *sz <= 0xBF)
+					sz += 3;
+				else
+				{
+					wide_val = (c & 0x07);
+					l = 3;
+				}
+				break;		
+			
+			default:
+				break;
 		}
-
-		for (int i=trailers; i>0; --i,++p)
+		
+		while (l-- > 0)
 		{
-			if (len == size_t(-1) ? *p=='\0' : size_t(p-sz)>=len)
+			c = *sz++;
+			if (utf8_data[c] != -1)
 			{
-				c = L'\xFFFD';
+				wide_val = utf_subst_val;
+				sz += l;
 				break;
 			}
-
-			if (c != L'\xFFFD')
-			{
-				c <<= 6;
-				c += (*p & 0x3f);
-			}
+			
+			wide_val <<= 6;
+			wide_val |= (c & 0x3F);
 		}
-
-		if (sizeof(wchar_t) == 2 && (c & 0xFFFF0000))
+		
+		if (sizeof(wchar_t) == 2 && (wide_val & 0xFFFF0000))
 		{
-			// Oh god.. we're big and going to UCS-16
+			// Oh god.. we're big and going to UTF-16
 			required_len += 2;
 		}
 		else
 			++required_len;
-	}
-
-	if (len == size_t(-1))
-		++required_len;
+		
+	} while (len == size_t(-1) ? c != '\0' : size_t(sz-start)<len);
 
 	return required_len;
 }
 
-size_t OOBase::from_utf8(wchar_t* wsz, size_t wlen, const char* sz, size_t len)
+size_t OOBase::from_utf8(wchar_t* wsz, size_t wlen, const char* start, size_t len)
 {
 	if (!wsz || !wlen)
-		return measure_utf8(sz,len);
+		return measure_utf8(start,len);
 
-	if (!sz)
+	if (!start)
 	{
 		if (len == size_t(-1))
 		{
@@ -274,64 +350,116 @@ size_t OOBase::from_utf8(wchar_t* wsz, size_t wlen, const char* sz, size_t len)
 	}
 
 	wchar_t* wp = wsz;
+	const char* sz = start;
 	size_t required_len = 0;
-	for (const char* p=sz; len == size_t(-1) ? *p!='\0' : size_t(p-sz)<len;)
+
+	unsigned char c;
+
+	do
 	{
-		unsigned int c;
-		unsigned char v = *p++;
-		int trailers = trailingBytesForUTF8[v];
-		switch (trailers)
+		// Default to substitution value
+		unsigned int wide_val = utf_subst_val;
+		int l = 0;
+
+		c = *sz++;
+		
+		switch (utf8_data[c])
 		{
-		case 0:
-			c = v;
-			break;
-
-		case 1:
-			c = v & 0x1f;
-			break;
-
-		case 2:
-			c = v & 0xf;
-			break;
-
-		case 3:
-			c = v & 0x7;
-			break;
-
-		default:
-			trailers &= 7;
-			c = L'\xFFFD';
+			case 1:
+				wide_val = c;
+				break;
+			
+			case 2: 
+				wide_val = (c & 0x1F);
+				l = 1;
+				break;
+			
+			case -2:
+				++sz;
+				break;
+				
+			case 3:
+				wide_val = (c & 0x0F);
+				l = 2;
+				break;
+			
+			case -3: 
+				if (*sz >= 0x80 && *sz <= 0x9F)
+					sz += 2;
+				else
+				{
+					wide_val = (c & 0x0F);
+					l = 2;
+				}
+				break;
+					
+			case -4:
+				if (*sz >= 0xA0 && *sz <= 0xBF)
+					sz += 2;
+				else
+				{
+					wide_val = (c & 0x0F);
+					l = 2;
+				}
+				break;
+				
+			case 4:
+				wide_val = (c & 0x07);
+				l = 3;
+				break;
+				
+			case -5:
+				if (*sz >= 0x80 && *sz <= 0x8F)
+					sz += 3;
+				else
+				{
+					wide_val = (c & 0x07);
+					l = 3;
+				}
+				break;
+			
+			case -6:
+				if (*sz >= 0x90 && *sz <= 0xBF)
+					sz += 3;
+				else
+				{
+					wide_val = (c & 0x07);
+					l = 3;
+				}
+				break;		
+			
+			default:
+				break;
 		}
-
-		for (int i=0; i<trailers; ++i,++p)
+		
+		while (l-- > 0)
 		{
-			if (len == size_t(-1) ? *p=='\0' : size_t(p-sz)>=len)
+			c = *sz++;
+			if (utf8_data[c] != -1)
 			{
-				c = L'\xFFFD';
+				wide_val = utf_subst_val;
+				sz += l;
 				break;
 			}
-
-			if (c != L'\xFFFD')
-			{
-				c <<= 6;
-				c += (*p & 0x3f);
-			}
+			
+			wide_val <<= 6;
+			wide_val |= (c & 0x3F);
 		}
-
-		if (sizeof(wchar_t) == 2 && (c & 0xFFFF0000))
+		
+		if (sizeof(wchar_t) == 2 && (wide_val & 0xFFFF0000))
 		{
-			// Oh god.. we're big and going to UCS-16
+			// Oh god.. we're big and going to UTF-16
 			required_len += 2;
-			c -= 0x10000;
+			wide_val -= 0x10000;
 
-			if (required_len < len)
+			if (required_len < wlen)
 			{
 #if (OMEGA_BYTE_ORDER != OMEGA_BIG_ENDIAN)
-				*wp++ = static_cast<wchar_t>((c >> 10) | 0xD800);
-				*wp++ = static_cast<wchar_t>((c & 0x3ff) | 0xDC00);
+				*wp++ = static_cast<wchar_t>((wide_val >> 10) | 0xD800);
+				*wp++ = static_cast<wchar_t>((wide_val & 0x3ff) | 0xDC00);
 #else
-				*wp++ = static_cast<wchar_t>((c & 0x3ff) | 0xDC00);
-				*wp++ = static_cast<wchar_t>((c >> 10) | 0xD800);
+				*wp++ = static_cast<wchar_t>((wide_val & 0x3ff) | 0xDC00);
+				*wp++ = static_cast<wchar_t>((wide_val >> 10) | 0xD800);
 #endif
 			}
 		}
@@ -339,16 +467,10 @@ size_t OOBase::from_utf8(wchar_t* wsz, size_t wlen, const char* sz, size_t len)
 		{
 			++required_len;
 			if (required_len < wlen)
-				*wp++ = static_cast<wchar_t>(c);
+				*wp++ = static_cast<wchar_t>(wide_val);
 		}
-	}
-
-	if (len == size_t(-1))
-	{
-		++required_len;
-		if (required_len < wlen)
-			*wp = L'\0';
-	}
+	} 
+	while (len == size_t(-1) ? c != '\0' : size_t(sz-start)<len);
 
 	return required_len;
 }
@@ -526,7 +648,7 @@ size_t OOBase::measure_native(const char* sz, size_t len)
 	wchar_t wc[2];
 	mbstate_t state = {0};
 	size_t required_len = 0;
-	size_t c = MB_CUR_MAX;
+	size_t c = MB_LEN_MAX;
 
 	for (const char* p=sz; size_t(p-sz)<len;)
 	{
@@ -581,7 +703,7 @@ size_t OOBase::from_native(wchar_t* wsz, size_t wlen, const char* sz, size_t len
 	wchar_t* wp = wsz;
 	mbstate_t state = {0};
 	size_t required_len = 0;
-	size_t c = MB_CUR_MAX;
+	size_t c = MB_LEN_MAX;
 
 	for (const char* p=sz; size_t(p-sz)<len && size_t(wp-wsz)<wlen;)
 	{
@@ -622,7 +744,7 @@ size_t OOBase::measure_native(const wchar_t* wsz, size_t len)
 	if (!wsz)
 		return (len == size_t(-1) ? 1 : 0);
 
-	char c[MB_CUR_MAX];
+	char c[MB_LEN_MAX];
 	mbstate_t state = {0};
 	size_t required_len = 0;
 	for (const wchar_t* wp=wsz; size_t(wp-wsz)<len;)
@@ -653,7 +775,7 @@ size_t OOBase::to_native(char* sz, size_t len, const wchar_t* wsz, size_t wlen)
 			return 0;
 	}
 
-	char c[MB_CUR_MAX];
+	char c[MB_LEN_MAX];
 	char* p = sz;
 	mbstate_t state = {0};
 	size_t required_len = 0;
