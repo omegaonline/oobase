70ad350a261c4c5be7e3e13e4526e9d1c3d1d42c
Rick Taylor <rick@tropicalstormsoftware.com>
2012-11-05 17:11:39 +0000

    Add generic INI file reader
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

---
+++ include/OOBase/ConfigFile.h
@@ -0,0 +1,48 @@
+///////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2012 Rick Taylor
+//
+// This file is part of OOBase, the Omega Online Base library.
+//
+// OOBase is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OOBase is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OOBase.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////////
+
+#ifndef OOBASE_CONFIGFILE_H_INCLUDED_
+#define OOBASE_CONFIGFILE_H_INCLUDED_
+
+#include "String.h"
+#include "Table.h"
+
+namespace OOBase
+{
+	namespace ConfigFile
+	{
+		typedef Table<String,String> results_t;
+
+		struct error_pos_t
+		{
+			size_t line;
+			size_t col;
+		};
+
+		int load(const char* filename, results_t& results, error_pos_t* error_pos);
+
+#if defined(_WIN32)
+		int load_registry(const char* key, results_t& results);
+#endif
+	};
+}
+
+#endif // OOBASE_CONFIGFILE_H_INCLUDED_
