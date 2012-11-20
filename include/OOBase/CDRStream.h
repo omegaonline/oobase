554c381ae4e4d71cf23d00f7a424583a5f87bfb4
Rick Taylor <rick@tropicalstormsoftware.com>
2012-11-20 17:01:02 +0000

    Use new alignof for CDRStream alignment
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- include/OOBase/CDRStream.h
+++ include/OOBase/CDRStream.h
@@ -169,7 +169,7 @@ namespace OOBase
 			if (m_last_error != 0 || !m_buffer)
 				return false;
 
-			m_buffer->align_rd_ptr(sizeof(T));
+			m_buffer->align_rd_ptr(detail::alignof<T>::value);
 			if (m_buffer->length() < sizeof(T))
 				return error_eof();
 
@@ -280,7 +280,7 @@ namespace OOBase
 			if (m_last_error != 0 || !m_buffer)
 				return false;
 
-			m_last_error = m_buffer->align_wr_ptr(sizeof(T));
+			m_last_error = m_buffer->align_wr_ptr(detail::alignof<T>::value);
 			if (m_last_error != 0)
 				return false;
 
