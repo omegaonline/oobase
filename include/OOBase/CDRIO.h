b970147fa0d1aaedaeee858c830116881c5c6816
Rick Taylor <rick@tropicalstormsoftware.com>
2013-01-23 14:06:28 +0000

    Add more helpers to CDRIO
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- include/OOBase/CDRIO.h
+++ include/OOBase/CDRIO.h
@@ -42,9 +42,38 @@ namespace OOBase
 			if (!thunk)
 				return ERROR_OUTOFMEMORY;
 
-			thunk->m_pSocket = pSocket;
-			thunk->m_pSocket->addref();
+			thunk->m_ptrSocket = pSocket;
 			return pSocket->recv(thunk,&ThunkRHS<T,H>::fn1,stream.buffer(),sizeof(H));
+		}
+
+		template <typename T, typename H>
+		static int recv_msg_with_header_sync(H expected_len, AsyncSocket* pSocket, T* param, void (T::*callback)(CDRStream& stream, Buffer* ctl_buffer, int err), Buffer* ctl_buffer)
+		{
+			CDRStream stream(expected_len);
+			if (stream.last_error())
+				return stream.last_error();
+
+			ThunkRMHS<T,H>* thunk = pSocket->thunk_allocate<ThunkRMHS<T,H>,T*,void (T::*)(CDRStream&,Buffer*,int)>(param,callback);
+			if (!thunk)
+				return ERROR_OUTOFMEMORY;
+
+			thunk->m_ptrSocket = pSocket;
+			return pSocket->recv_msg(thunk,&ThunkRMHS<T,H>::fn1,stream.buffer(),ctl_buffer,sizeof(H));
+		}
+
+		template <typename T, typename H>
+		static int send_and_recv_with_header_sync(H expected_len, CDRStream& stream, AsyncSocket* pSocket, T* param, void (T::*callback)(CDRStream& stream, int err))
+		{
+			int err = stream.buffer()->space(expected_len);
+			if (err)
+				return err;
+
+			ThunkSRHS<T,H>* thunk = pSocket->thunk_allocate<ThunkSRHS<T,H>,T*,void (T::*)(CDRStream&,int)>(param,callback);
+			if (!thunk)
+				return ERROR_OUTOFMEMORY;
+
+			thunk->m_ptrSocket = pSocket;
+			return pSocket->send(thunk,&ThunkSRHS<T,H>::fn1,stream.buffer());
 		}
 
 	private:
@@ -52,13 +81,13 @@ namespace OOBase
 		struct ThunkRHS
 		{
 			ThunkRHS(T* param, void (T::*callback)(CDRStream&,int), AllocatorInstance& allocator) :
-				m_param(param),m_callback(callback),m_allocator(allocator),m_pSocket(NULL)
+				m_param(param),m_callback(callback),m_allocator(allocator)
 			{}
 
 			T* m_param;
 			void (T::*m_callback)(CDRStream&,int);
-			AllocatorInstance& m_allocator;
-			AsyncSocket* m_pSocket;
+			AllocatorInstance&  m_allocator;
+			RefPtr<AsyncSocket> m_ptrSocket;
 
 			static void fn1(void* param, Buffer* buffer, int err)
 			{
@@ -70,11 +99,10 @@ namespace OOBase
 					if (!stream.read(msg_len))
 						err = stream.last_error();
 					else
-						err = thunk.m_pSocket->recv(param,&fn2,buffer,msg_len - sizeof(H));
+						err = thunk.m_ptrSocket->recv(param,&fn2,buffer,msg_len - sizeof(H));
 				}
 				if (err)
 				{
-					thunk.m_pSocket->release();
 					static_cast<ThunkRHS*>(param)->~ThunkRHS();
 					thunk.m_allocator.free(param);
 					(thunk.m_param->*thunk.m_callback)(stream,err);
@@ -84,8 +112,109 @@ namespace OOBase
 			static void fn2(void* param, Buffer* buffer, int err)
 			{
 				ThunkRHS thunk = *static_cast<ThunkRHS*>(param);
-				thunk.m_pSocket->release();
 				static_cast<ThunkRHS*>(param)->~ThunkRHS();
+				thunk.m_allocator.free(param);
+				CDRStream stream(buffer);
+				(thunk.m_param->*thunk.m_callback)(stream,err);
+			}
+		};
+
+		template <typename T, typename H>
+		struct ThunkRMHS
+		{
+			ThunkRMHS(T* param, void (T::*callback)(CDRStream&,Buffer*,int), AllocatorInstance& allocator) :
+				m_param(param),m_callback(callback),m_allocator(allocator)
+			{}
+
+			T* m_param;
+			void (T::*m_callback)(CDRStream&,Buffer*,int);
+			AllocatorInstance&  m_allocator;
+			RefPtr<AsyncSocket> m_ptrSocket;
+
+			static void fn1(void* param, Buffer* data_buffer, Buffer* ctl_buffer, int err)
+			{
+				CDRStream stream(data_buffer);
+				ThunkRMHS thunk = *static_cast<ThunkRMHS*>(param);
+				if (!err)
+				{
+					H msg_len = 0;
+					if (!stream.read(msg_len))
+						err = stream.last_error();
+					else
+						err = thunk.m_ptrSocket->recv_msg(param,&fn2,data_buffer,ctl_buffer,msg_len - sizeof(H));
+				}
+				if (err)
+				{
+					static_cast<ThunkRMHS*>(param)->~ThunkRMHS();
+					thunk.m_allocator.free(param);
+					(thunk.m_param->*thunk.m_callback)(stream,ctl_buffer,err);
+				}
+			}
+
+			static void fn2(void* param, Buffer* data_buffer, Buffer* ctl_buffer, int err)
+			{
+				ThunkRMHS thunk = *static_cast<ThunkRMHS*>(param);
+				static_cast<ThunkRMHS*>(param)->~ThunkRMHS();
+				thunk.m_allocator.free(param);
+				CDRStream stream(data_buffer);
+				(thunk.m_param->*thunk.m_callback)(stream,ctl_buffer,err);
+			}
+		};
+
+		template <typename T, typename H>
+		struct ThunkSRHS
+		{
+			ThunkSRHS(T* param, void (T::*callback)(CDRStream&,int), AllocatorInstance& allocator) :
+				m_param(param),m_callback(callback),m_allocator(allocator)
+			{}
+
+			T* m_param;
+			void (T::*m_callback)(CDRStream&,int);
+			AllocatorInstance&  m_allocator;
+			RefPtr<AsyncSocket> m_ptrSocket;
+
+			static void fn1(void* param, Buffer* buffer, int err)
+			{
+				ThunkSRHS thunk = *static_cast<ThunkSRHS*>(param);
+				if (!err)
+				{
+					err = buffer->reset();
+					if (!err)
+						err = thunk.m_ptrSocket->recv(param,&fn2,buffer,sizeof(H));
+				}
+				if (err)
+				{
+					static_cast<ThunkSRHS*>(param)->~ThunkSRHS();
+					thunk.m_allocator.free(param);
+					CDRStream stream(buffer);
+					(thunk.m_param->*thunk.m_callback)(stream,err);
+				}
+			}
+
+			static void fn2(void* param, Buffer* buffer, int err)
+			{
+				CDRStream stream(buffer);
+				ThunkSRHS thunk = *static_cast<ThunkSRHS*>(param);
+				if (!err)
+				{
+					H msg_len = 0;
+					if (!stream.read(msg_len))
+						err = stream.last_error();
+					else
+						err = thunk.m_ptrSocket->recv(param,&fn3,buffer,msg_len - sizeof(H));
+				}
+				if (err)
+				{
+					static_cast<ThunkSRHS*>(param)->~ThunkSRHS();
+					thunk.m_allocator.free(param);
+					(thunk.m_param->*thunk.m_callback)(stream,err);
+				}
+			}
+
+			static void fn3(void* param, Buffer* buffer, int err)
+			{
+				ThunkSRHS thunk = *static_cast<ThunkSRHS*>(param);
+				static_cast<ThunkSRHS*>(param)->~ThunkSRHS();
 				thunk.m_allocator.free(param);
 				CDRStream stream(buffer);
 				(thunk.m_param->*thunk.m_callback)(stream,err);
