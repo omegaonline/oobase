cde7bbd6930a72ffe1f396dec0385bb45685c9ea
Rick Taylor <rick@tropicalstormsoftware.com>
2015-04-25 13:55:36 +0100

    Add Delegate class an update Signals to use them
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

---
+++ include/OOBase/Delegate.h
@@ -0,0 +1,225 @@
+///////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2015 Rick Taylor
+//
+// This file is part of OOBase, the Omega Online Base library.
+//
+// OOBase is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OOBase is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OOBase.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////////
+
+#ifndef OOBASE_DELEGATE_H_INCLUDED_
+#define OOBASE_DELEGATE_H_INCLUDED_
+
+#include "SharedPtr.h"
+
+namespace OOBase
+{
+	template <typename Allocator = CrtAllocator>
+	class Delegate0
+	{
+	public:
+		template<typename T>
+		Delegate0(const WeakPtr<T>& ptr, void (T::*fn)()) : m_static(NULL)
+		{
+			m_ptr = OOBase::allocate_shared<Thunk<T>,Allocator>(ptr,fn);
+		}
+
+		Delegate0(void (*fn)()) : m_static(fn)
+		{
+		}
+
+		bool operator == (const Delegate0& rhs) const
+		{
+			if (this == &rhs)
+				return true;
+			return m_ptr == rhs.m_ptr && m_static == rhs.m_static;
+		}
+
+		bool invoke() const
+		{
+			if (m_ptr)
+				return m_ptr->thunk();
+
+			(*m_static)();
+			return true;
+		}
+
+		void swap(Delegate0& rhs)
+		{
+			m_ptr.swap(rhs.m_ptr);
+			OOBase::swap(m_static,rhs.m_static);
+		}
+
+	private:
+		struct ThunkBase
+		{
+			virtual bool thunk() = 0;
+		};
+
+		template<typename T>
+		struct Thunk : public ThunkBase
+		{
+			Thunk(const WeakPtr<T>& ptr, void (T::*fn)()) : m_ptr(ptr), m_fn(fn)
+			{}
+
+			bool thunk()
+			{
+				if (!m_ptr)
+					return false;
+
+				(m_ptr.lock().get()->*m_fn)();
+				return true;
+			}
+
+			WeakPtr<T> m_ptr;
+			void (T::*m_fn)();
+		};
+
+		SharedPtr<ThunkBase> m_ptr;
+		void (*m_static)();
+	};
+
+	template <typename P1, typename Allocator = CrtAllocator>
+	class Delegate1
+	{
+	public:
+		template<typename T>
+		Delegate1(const WeakPtr<T>& ptr, void (T::*fn)(const P1&)) : m_static(NULL)
+		{
+			m_ptr = OOBase::allocate_shared<Thunk<T>,Allocator>(ptr,fn);
+		}
+
+		Delegate1(void (*fn)(const P1&)) : m_static(fn)
+		{
+		}
+
+		bool operator == (const Delegate1& rhs) const
+		{
+			if (this == &rhs)
+				return true;
+			return m_ptr == rhs.m_ptr && m_static == rhs.m_static;
+		}
+
+		bool invoke(const P1& p1) const
+		{
+			if (m_ptr)
+				return m_ptr->thunk(p1);
+
+			(*m_static)(p1);
+			return true;
+		}
+
+		void swap(Delegate1& rhs)
+		{
+			m_ptr.swap(rhs.m_ptr);
+			OOBase::swap(m_static,rhs.m_static);
+		}
+
+	private:
+		struct ThunkBase
+		{
+			virtual bool thunk(const P1&) = 0;
+		};
+
+		template<typename T>
+		struct Thunk : public ThunkBase
+		{
+			Thunk(const WeakPtr<T>& ptr, void (T::*fn)(const P1&)) : m_ptr(ptr), m_fn(fn)
+			{}
+
+			bool thunk(const P1& p1)
+			{
+				if (!m_ptr)
+					return false;
+
+				(m_ptr.lock().get()->*m_fn)(p1);
+				return true;
+			}
+
+			WeakPtr<T> m_ptr;
+			void (T::*m_fn)(const P1&);
+		};
+
+		SharedPtr<ThunkBase> m_ptr;
+		void (*m_static)(const P1&);
+	};
+
+	template <typename P1, typename P2, typename Allocator = CrtAllocator>
+	class Delegate2
+	{
+	public:
+		template<typename T>
+		Delegate2(const WeakPtr<T>& ptr, void (T::*fn)(const P1&,const P2&)) : m_static(NULL)
+		{
+			m_ptr = OOBase::allocate_shared<Thunk<T>,Allocator>(ptr,fn);
+		}
+
+		Delegate2(void (*fn)(const P1&,const P2&)) : m_static(fn)
+		{
+		}
+
+		bool operator == (const Delegate2& rhs) const
+		{
+			if (this == &rhs)
+				return true;
+			return m_ptr == rhs.m_ptr && m_static == rhs.m_static;
+		}
+
+		bool invoke(const P1& p1, const P2& p2) const
+		{
+			if (m_ptr)
+				return m_ptr->thunk(p1,p2);
+
+			(*m_static)(p1,p2);
+			return true;
+		}
+
+		void swap(Delegate2& rhs)
+		{
+			m_ptr.swap(rhs.m_ptr);
+			OOBase::swap(m_static,rhs.m_static);
+		}
+
+	private:
+		struct ThunkBase
+		{
+			virtual bool thunk(const P1&, const P2&) = 0;
+		};
+
+		template<typename T>
+		struct Thunk : public ThunkBase
+		{
+			Thunk(const WeakPtr<T>& ptr, void (T::*fn)(const P1&,const P2&)) : m_ptr(ptr), m_fn(fn)
+			{}
+
+			bool thunk(const P1& p1, const P2& p2)
+			{
+				if (!m_ptr)
+					return false;
+
+				(m_ptr.lock().get()->*m_fn)(p1,p2);
+				return true;
+			}
+
+			WeakPtr<T> m_ptr;
+			void (T::*m_fn)(const P1&,const P2&);
+		};
+
+		SharedPtr<ThunkBase> m_ptr;
+		void (*m_static)(const P1&,const P2&);
+	};
+}
+
+#endif // OOBASE_DELEGATE_H_INCLUDED_
--- include/OOBase/SignalSlot.h
+++ include/OOBase/SignalSlot.h
@@ -22,7 +22,7 @@
 #ifndef OOBASE_SIGNALSLOT_H_INCLUDED_
 #define OOBASE_SIGNALSLOT_H_INCLUDED_
 
-#include "SharedPtr.h"
+#include "Delegate.h"
 #include "Vector.h"
 
 namespace OOBase
@@ -31,70 +31,8 @@ namespace OOBase
 	class Signal0
 	{
 	private:
-		class Slot
-		{
-		public:
-			template<typename T>
-			Slot(const WeakPtr<T>& ptr, void (T::*fn)()) : m_static(NULL)
-			{
-				m_ptr = OOBase::allocate_shared<Thunk<T>,Allocator>(ptr,fn);
-			}
-
-			Slot(void (*fn)()) : m_static(fn)
-			{
-			}
-
-			bool operator == (const Slot& rhs) const
-			{
-				if (this == &rhs)
-					return true;
-				return m_ptr == rhs.m_ptr && m_static == rhs.m_static;
-			}
-
-			bool invoke() const
-			{
-				if (m_ptr)
-					return m_ptr->thunk();
-
-				(*m_static)();
-				return true;
-			}
-
-			void swap(Slot& rhs)
-			{
-				m_ptr.swap(rhs.m_ptr);
-				OOBase::swap(m_static,rhs.m_static);
-			}
-
-		private:
-			struct ThunkBase
-			{
-				virtual bool thunk() = 0;
-			};
-
-			SharedPtr<ThunkBase> m_ptr;
-			void (*m_static)();
-
-			template<typename T>
-			struct Thunk : public ThunkBase
-			{
-				Thunk(const WeakPtr<T>& ptr, void (T::*fn)()) : m_ptr(ptr), m_fn(fn)
-				{}
-
-				bool thunk()
-				{
-					if (!m_ptr)
-						return false;
-
-					(m_ptr.lock().get()->*m_fn)();
-					return true;
-				}
-
-				WeakPtr<T> m_ptr;
-				void (T::*m_fn)();
-			};
-		};
-		mutable Vector<Slot,Allocator> m_slots;
+		typedef Delegate0<Allocator> delegate_t;
+		mutable Vector<delegate_t,Allocator> m_slots;
 
 	public:
 		Signal0()
@@ -106,24 +44,24 @@ namespace OOBase
 		template <typename T>
 		int connect(const WeakPtr<T>& ptr, void (T::*slot)())
 		{
-			return m_slots.push_back(Slot(ptr,slot));
+			return m_slots.push_back(delegate_t(ptr,slot));
 		}
 
 		int connect(void (*slot)())
 		{
-			return m_slots.push_back(Slot(slot));
+			return m_slots.push_back(delegate_t(slot));
 		}
 
 		template <typename T>
 		bool disconnect(WeakPtr<T>& ptr, void (T::*slot)())
 		{
-			return m_slots.remove(Slot(ptr,slot));
+			return m_slots.remove(delegate_t(ptr,slot));
 		}
 
 		void fire() const
 		{
-			Vector<Slot,Allocator> slots(m_slots);
-			for (typename Vector<Slot,Allocator>::iterator i=slots.begin();i!=slots.end();++i)
+			Vector<delegate_t,Allocator> slots(m_slots);
+			for (typename Vector<delegate_t,Allocator>::iterator i=slots.begin();i!=slots.end();++i)
 			{
 				if (!i->invoke())
 					m_slots.erase(*i);
@@ -135,70 +73,8 @@ namespace OOBase
 	class Signal1
 	{
 	private:
-		class Slot
-		{
-		public:
-			template<typename T>
-			Slot(const WeakPtr<T>& ptr, void (T::*fn)(const P1& p1)) : m_static(NULL)
-			{
-				m_ptr = OOBase::allocate_shared<Thunk<T>,Allocator>(ptr,fn);
-			}
-
-			Slot(void (*fn)(const P1& p1)) : m_static(fn)
-			{
-			}
-
-			bool operator == (const Slot& rhs) const
-			{
-				if (this == &rhs)
-					return true;
-				return m_ptr == rhs.m_ptr && m_static == rhs.m_static;
-			}
-
-			bool invoke(const P1& p1) const
-			{
-				if (m_ptr)
-					return m_ptr->thunk(p1);
-
-				(*m_static)(p1);
-				return true;
-			}
-
-			void swap(Slot& rhs)
-			{
-				m_ptr.swap(rhs.m_ptr);
-				OOBase::swap(m_static,rhs.m_static);
-			}
-
-		private:
-			struct ThunkBase
-			{
-				virtual bool thunk(const P1& p1) = 0;
-			};
-
-			SharedPtr<ThunkBase> m_ptr;
-			void (*m_static)(const P1& p1);
-
-			template<typename T>
-			struct Thunk : public ThunkBase
-			{
-				Thunk(const WeakPtr<T>& ptr, void (T::*fn)(const P1& p1)) : m_ptr(ptr), m_fn(fn)
-				{}
-
-				bool thunk(const P1& p1)
-				{
-					if (!m_ptr)
-						return false;
-
-					(m_ptr.lock().get()->*m_fn)(p1);
-					return true;
-				}
-
-				WeakPtr<T> m_ptr;
-				void (T::*m_fn)(const P1& p1);
-			};
-		};
-		mutable Vector<Slot,Allocator> m_slots;
+		typedef Delegate1<P1,Allocator> delegate_t;
+		mutable Vector<delegate_t,Allocator> m_slots;
 
 	public:
 		Signal1()
@@ -208,26 +84,26 @@ namespace OOBase
 		{}
 
 		template <typename T>
-		int connect(const WeakPtr<T>& ptr, void (T::*slot)(const P1& p1))
+		int connect(const WeakPtr<T>& ptr, void (T::*slot)(const P1&))
 		{
-			return m_slots.push_back(Slot(ptr,slot));
+			return m_slots.push_back(delegate_t(ptr,slot));
 		}
 
-		int connect(void (*slot)(const P1& p1))
+		int connect(void (*slot)(const P1&))
 		{
-			return m_slots.push_back(Slot(slot));
+			return m_slots.push_back(delegate_t(slot));
 		}
 
 		template <typename T>
-		bool disconnect(WeakPtr<T>& ptr, void (T::*slot)(const P1& p1))
+		bool disconnect(WeakPtr<T>& ptr, void (T::*slot)(const P1&))
 		{
-			return m_slots.remove(Slot(ptr,slot));
+			return m_slots.remove(delegate_t(ptr,slot));
 		}
 
 		void fire(const P1& p1) const
 		{
-			Vector<Slot,Allocator> slots(m_slots);
-			for (typename Vector<Slot,Allocator>::iterator i=slots.begin();i!=slots.end();++i)
+			Vector<delegate_t,Allocator> slots(m_slots);
+			for (typename Vector<delegate_t,Allocator>::iterator i=slots.begin();i!=slots.end();++i)
 			{
 				if (!i->invoke(p1))
 					m_slots.erase(*i);
@@ -239,70 +115,8 @@ namespace OOBase
 	class Signal2
 	{
 	private:
-		class Slot
-		{
-		public:
-			template<typename T>
-			Slot(const WeakPtr<T>& ptr, void (T::*fn)(const P1& p1, const P2& p2)) : m_static(NULL)
-			{
-				m_ptr = OOBase::allocate_shared<Thunk<T>,Allocator>(ptr,fn);
-			}
-
-			Slot(void (*fn)(const P1& p1, const P2& p2)) : m_static(fn)
-			{
-			}
-
-			bool operator == (const Slot& rhs) const
-			{
-				if (this == &rhs)
-					return true;
-				return m_ptr == rhs.m_ptr && m_static == rhs.m_static;
-			}
-
-			bool invoke(const P1& p1, const P2& p2) const
-			{
-				if (m_ptr)
-					return m_ptr->thunk(p1,p2);
-
-				(*m_static)(p1,p2);
-				return true;
-			}
-
-			void swap(Slot& rhs)
-			{
-				m_ptr.swap(rhs.m_ptr);
-				OOBase::swap(m_static,rhs.m_static);
-			}
-
-		private:
-			struct ThunkBase
-			{
-				virtual bool thunk(const P1& p1, const P2& p2) = 0;
-			};
-
-			SharedPtr<ThunkBase> m_ptr;
-			void (*m_static)(const P1& p1, const P2& p2);
-
-			template<typename T>
-			struct Thunk : public ThunkBase
-			{
-				Thunk(const WeakPtr<T>& ptr, void (T::*fn)(const P1& p1, const P2& p2)) : m_ptr(ptr), m_fn(fn)
-				{}
-
-				bool thunk(const P1& p1, const P2& p2)
-				{
-					if (!m_ptr)
-						return false;
-
-					(m_ptr.lock().get()->*m_fn)(p1,p2);
-					return true;
-				}
-
-				WeakPtr<T> m_ptr;
-				void (T::*m_fn)(const P1& p1, const P2& p2);
-			};
-		};
-		mutable Vector<Slot,Allocator> m_slots;
+		typedef Delegate2<P1,P2,Allocator> delegate_t;
+		mutable Vector<delegate_t,Allocator> m_slots;
 
 	public:
 		Signal2()
@@ -312,26 +126,26 @@ namespace OOBase
 		{}
 
 		template <typename T>
-		int connect(const WeakPtr<T>& ptr, void (T::*slot)(const P1& p1,const P2& p2))
+		int connect(const WeakPtr<T>& ptr, void (T::*slot)(const P1&,const P2&))
 		{
-			return m_slots.push_back(Slot(ptr,slot));
+			return m_slots.push_back(delegate_t(ptr,slot));
 		}
 
-		int connect(void (*slot)(const P1& p1, const P2& p2))
+		int connect(void (*slot)(const P1&,const P2&))
 		{
-			return m_slots.push_back(Slot(slot));
+			return m_slots.push_back(delegate_t(slot));
 		}
 
 		template <typename T>
-		bool disconnect(WeakPtr<T>& ptr, void (T::*slot)(const P1& p1,const P2& p2))
+		bool disconnect(WeakPtr<T>& ptr, void (T::*slot)(const P1&,const P2&))
 		{
-			return m_slots.remove(Slot(ptr,slot));
+			return m_slots.remove(delegate_t(ptr,slot));
 		}
 
 		void fire(const P1& p1, const P2& p2) const
 		{
-			Vector<Slot,Allocator> slots(m_slots);
-			for (typename Vector<Slot,Allocator>::iterator i=slots.begin();i!=slots.end();++i)
+			Vector<delegate_t,Allocator> slots(m_slots);
+			for (typename Vector<delegate_t,Allocator>::iterator i=slots.begin();i!=slots.end();++i)
 			{
 				if (!i->invoke(p1,p2))
 					m_slots.erase(*i);
